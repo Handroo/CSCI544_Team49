3ccddc4f807e876c0bf4f01201188e35d46275de
Handroo <hanandre@usc.edu>
2015-04-24 18:58:19 -0700

    first upload of my code

--- SentenceGenerator/SentenceGenerator.py
+++ SentenceGenerator/SentenceGenerator.py
@@ -1,37 +1,46 @@
 import sys
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import sentiwordnet as swn
+from nltk.corpus import wordnet as wn
 from random import randint
 
+adjOverload = False
+adjOverloadList = []
 sentenceFile= open(sys.argv[1], 'r')
-
-startList = ['The ','Students say ','Most of the time, ','You will notice that ']
+lastRand = -1
+startList = ['', '','','', '','', 'The ', 'The ','The ', 'The ', 'The ','Students think ','Overall, ','Generally, ','Reviews say ']
 posAdvList = ['','fortunately ', 'favorably ','conveniently ']
 negAdvList = ['','unfortunately ', 'unluckily ']
+
 
 def main():
     line = sentenceFile.readline()
     while line != "":
-        line = line.replace("\n","")
-        boolean = isProfessor(line)
-        if(not boolean):
-            print(makeSentence(line),end=" ")
+        if line != "\n":
+            # print("S:"+line)
+            line = line.replace("\n","")
+            boolean = isProfessor(line)
+            if(not boolean):
+                print(makeSentence(line),end=" ")
+            else:
+                print("\n"+line)
         line = sentenceFile.readline()
     print("")
 
 def sentimentAnalyzer(word):
+    #Potential to fill later if we want to use sentiment analyzer
     result = 'neutral'
-    list(swn.senti_synsets('slow'))
-    opinion = swn.senti_synsets(word)
-    opinion0 = list(opinion)[0]
-    posScore = opinion0.pos_score()
-    negScore =opinion0.neg_score()
-    if(abs(posScore - negScore) > .4):
-        if(posScore>negScore):
-            result = 'positive'
-        else:
-            result = 'negative'
-
+    # print("word: "+word)
+    # opinions = swn.senti_synsets(word)
+    # opinion = list(opinions)[0]
+    # posScore = opinion.pos_score()
+    # negScore =opinion.neg_score()
+    # if(abs(posScore - negScore) > .4):
+    #     if(posScore>negScore):
+    #         result = 'positive'
+    #     else:
+    #         result = 'negative'
+    # print(result)
     return result
 
 def adverbPhrase(adjective):
@@ -52,9 +61,19 @@ def randAdverb(opinion):
         index = randint(0,len(negAdvList)-1)
         return negAdvList[index]
 
-def randStart():
-    index = randint(0,len(startList)-1)
-    return startList[index]
+def randStart(word):
+    global lastRand
+    if word.lower() == "he" or word.lower() == "she" or word.lower() == "it":
+        return word[:1].upper() + word[1:]
+    else:
+        index = randint(0,len(startList)-1)
+        while index == lastRand:
+            index = randint(0,len(startList)-1)
+        lastRand = index
+        if index > 5:
+            return (startList[index] + word)
+        else:
+            return (startList[index] + word[:1].upper() + word[1:])
 
 def isplural(word):
     wnl = WordNetLemmatizer()
@@ -62,36 +81,142 @@ def isplural(word):
     plural = True if word is not lemma else False
     return plural
 
-def makeSentence(sentence):
-    sentenceToken  = sentence.split()
-    plural = isplural(sentenceToken[0])
-    if(not plural):
-        sentenceBuilder = randStart() + sentenceToken[0] + " is " + checkNeg(sentenceToken[1])
-    else:
-        sentenceBuilder = randStart() + sentenceToken[0] + " are " + checkNeg(sentenceToken[1])
+def filterAdj(adjList):
 
-    if(len(sentenceToken)>2):
-        for i in range(2,len(sentenceToken)-1):
-            sentenceBuilder += ", " +checkNeg(sentenceToken[i])
-        sentenceBuilder += " and "+ checkNeg(sentenceToken[len(sentenceToken)-1])
-    sentenceBuilder+="."
+    #lets remove synonyms first
+    blacklistAdj = {}
+    newAdjList = []
+    for i in range(0,len(adjList)):
+        word = adjList[i].split(':', 1)[0]
+        count = adjList[i].split(':', 1)[1]
+        # print("Analyzing "+word)
+        if word[0] != "~":#is synonym
+            # print("word: "+word)
+            synList = getSynonyms(word)#list of synonyms
+            valid = True
+            for syn in synList:#for all the synonyms
+                if syn in blacklistAdj:# ifsynonym is in blacklist
+                    valid = False
+            if word not in blacklistAdj and valid:
+                newAdjList.append(adjList[i])
+                for syn in synList:
+                    # print("Adding to bl: "+syn)
+                    blacklistAdj[syn] = count
+        else:#if negative, get the antonyms and see if any are in blacklist or in the adjlist
+            antList = getAntonyms(word[1:])
+            blacklisted = False
+            for ant in antList:
+                if ant in blacklistAdj:
+                    blacklisted = True
+                else:
+                    blacklistAdj[ant] = count
+
+
+            if not blacklisted:
+                newAdjList.append(adjList[i])
+
+    #if we still ahev too much, then cut it down 
+    if(len(newAdjList)>3):
+        slimAdjList = []
+        for adj in newAdjList:
+            count = adj.split(':', 1)[1]
+            if(count != '1'):
+                slimAdjList.append(adj)
+        if len(slimAdjList) == 0:
+            newAdjList = newAdjList[:4]
+        else:
+            newAdjList = slimAdjList
+
+
+    return newAdjList
+def adjOverload(overloadList):
+    global adjOverload, adjOverloadList
+    adjOverload = True
+    adjOverloadList = overloadList
+
+
+def makeSentence(sentence):
+
+    adjList = []
+    noun = ""
+    sentenceToken  = sentence.split()
+    noun = sentenceToken[0]
+    for i in range(2,len(sentenceToken)):
+        adjList.append(sentenceToken[i])
+    adjList = filterAdj(adjList)
+
+    plural = isplural(noun)#check if noun is plural
+    
+    if(not plural):
+        sentenceBuilder = randStart(noun) + " is " + checkNeg(adjList[0])
+    else:
+        sentenceBuilder = randStart(noun) + " are " + checkNeg(adjList[0])
+
+    if(len(adjList)>1):
+        for i in range(1,len(adjList)-1):
+            sentenceBuilder += ", " +checkNeg(adjList[i])
+        sentenceBuilder += " and "+ checkNeg(adjList[len(adjList)-1])
+    sentenceBuilder+=". "
+    # if adjOverload:
+    #     sentenceBuilder+=" Others "
     return sentenceBuilder
 
 def isProfessor(sentence):
-    if sentence == "__Professor__":
+    sentenceTokens = sentence.split()
+    if len(sentenceTokens) == 4 and sentenceTokens[0].isdigit():
         return True
     else:
         return False
 
 def checkNeg(word):
+    # print(word)
+    emphasis = ""
+    count = word.split(':', 1)[1]
+    if count == '2':
+        emphasis = "very"
+    elif count != '1':
+        emphasis = "super"
+    word = word.split(':', 1)[0]
     if word[0] == "~":
         return ("not "+word[1:])
     else:
         phrase = adverbPhrase(word)
-        return phrase
+        return emphasis+" " + phrase
+
+def getAntonyms(word):
+    allantonyms = {}
+    antonyms = []
+    for w in wn.synsets(word):
+        lemmas = w.lemmas()[0].antonyms()
+        for antonym in lemmas:
+            antonyms.append(str(antonym.key()).split('%',1)[0])
+        for a in antonyms:
+            if a not in allantonyms:
+                allantonyms[a] = a
+    return allantonyms
+
+def getSynonyms(word):
+    allsynonyms = {}
+    for s in  wn.synsets(word):
+        s = s.name().split(".")[0]
+        if s not in allsynonyms:
+            allsynonyms[s] = s
+    return allsynonyms
+
+def isSimilar(word1):
+    total = 0
+    tot = []
+    list1 = getAntonyms(word1)
+    for l1 in list1:
+    #     tot+=getSynonyms(l1)
+    # for t in tot:
+        print(l1)
+
+
+    
+
+
 
 if __name__ == "__main__":
     main()
-    # result = sentimentAnalyzer('difficult')
-    # print(result)
-    sentenceFile.close()
+    
